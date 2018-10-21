1bcec9f28e7203979ae66967798f975ebc518972
Connor <connormullett@gmail.com>
2018-10-20 23:42:13 -0400

    finished basic unit testing

--- test_main.py
+++ test_main.py
@@ -8,24 +8,26 @@ from phonebook import *
 
 class PhoneBookTest(unittest.TestCase):  # derives from unittest as stated above
 
+    # setup and teardown are known as test fixtures
     def setUp(self):  # will run before every test method
         self.phonebook = Phonebook()
 
+    def tearDown(self):  # will run for cleanup after each test
+        pass  # here we don't need to 'tearDown' anything, so its just pass
+        # this will still run EVEN if an exception is thrown
+
     def test_create_phonebook(self):
-        pbook = Phonebook()
+        pass
 
     def test_lookup_entry_by_name(self):
-        pbook = Phonebook()
-        pbook.add('bob', '12345')
-        self.assertEqual('12345', pbook.lookup('bob'))  # test will pass if the parameters are equal
+        self.phonebook.add('bob', '12345')
+        self.assertEqual('12345', self.phonebook.lookup('bob'))  # test will pass if the parameters are equal
         # or 'Assert' that these two values (expected, actual) are equal
 
     def test_missing_entry_raises_keyerror(self):
-        phonebook = Phonebook()
         with self.assertRaises(KeyError):  # assert that this method raises an exception
-            phonebook.lookup("missing")
+            self.phonebook.lookup("missing")
 
     @unittest.skip("Work in progress")  # tells python test runner to ignore this function as it is "work in progress"
     def test_empty_phonebook_is_consistent(self):
-        phonebook = Phonebook()
-        self.assertTrue(phonebook.is_consistent())  # assert that the parameters are true
+        self.assertTrue(self.phonebook.is_consistent())  # assert that the parameters are true
